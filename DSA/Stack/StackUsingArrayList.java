eedd3599094eb7fe9c632159576bb481d5e8d35c
Salauddin Ansari <salauddinansari07457@gmail.com>
2025-08-28 21:53:49 +0530

    add peek and pop method

--- DSA/Stack/StackUsingArrayList.java
+++ DSA/Stack/StackUsingArrayList.java
@@ -12,10 +12,20 @@ public class StackUsingArrayList<T> {
     public void push(T data){
         li.add(data);
     }
+    public T pop(){
+        if(isEmpty()) throw new RuntimeException("Stack is empty");
+        return li.remove(li.size()-1);
+    }
+    public T peek(){
+         if(isEmpty()) throw new RuntimeException("Stack is empty");
+        return li.get(li.size()-1);
+    }
 
     public static void main(String[] args) {
         StackUsingArrayList<Integer> st=new StackUsingArrayList<>();
         st.push(1);
         st.push(2);
+        System.out.println(st.pop());
+        System.out.println(st.pop());
     }
 }
